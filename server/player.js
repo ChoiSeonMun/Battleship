aad995efe2c1461d487e809b42ccd667556509d3
sulrang <angelace1007@gmail.com>
2019-11-23 08:16:39 +0900

    Refactored

--- server/player.js
+++ server/player.js
@@ -1,10 +1,30 @@
 class Player {
-    constructor(sock, name) {
-        this.id = sock.id;
+    constructor(sock) {
+        this.ID = sock.id;
         this.name = name;
         this.isReady = false;
         this.socket = sock;
         this.oppo = undefined;
+        this.session = undefined;
+    }
+
+    on(event, callback) {
+        this.socket.on(event, callback);
+    }
+
+    emit(event, msg) {
+        this.socket.emit(event, msg);
+    }
+
+    join(session) {
+        this.socket.join(session.ID, () => {
+            this.session = session.ID;
+            console.log(`${ player.ID } is in ${ session.ID }`);
+        });
+    }
+
+    close() {
+        setTimeout(() => this.socket.disconnect(true), 5000);
     }
 }
 module.exports.Player=Player;
--- server/session.js
+++ server/session.js
@@ -2,93 +2,86 @@ const protocol = require('../Common/protocol.js').protocol;
 
 class Session {
     constructor(player) {
-        // 플레이어의 id로 room을 만들어 join 시킨다.
-        player.socket.join(player.id, () => {
-            let rooms = Object.keys(player.socket.rooms);
-            console.log(`${ player.id } is in ${ rooms }`);
-        });
-
-        // 기본 세팅한다.
-        this.players = [];
+        this.ID = player.ID;
+        this.players = {};
+        this.players[player.ID] = player;
         this.host = player;
-        this.room = player.id;
+        this.isOver = false;
 
-        this.players.push(player);
+        // 플레이어를 조인시킨다.
+        player.join(this.roomID);
     }
 
-    // 적군을 추가한다.
     join(player) {
-        // 적군을 조인시킨다.
-        player.socket.join(this.room, () => {
-            let rooms = Object.keys(player.socket.rooms);
-            console.log(`${ player.id } is in ${ rooms }`);
-        });
-
+        // 유저를 해당 세션에 조인시킨다.
+        player.join(this.ID);
+        
         // 정보를 설정한다.
         this.host.oppo = player;
         player.oppo = this.host;
         this.players.push(player);
 
-        player.socket.emit('join_response', protocol.join_response(true));
+        // 상대방에게 조인이 완료되었다는 메시지를 보낸다.
+        player.emit('join_response', protocol.join_response(true));
         console.log(`${player.name} join successed`);
     }
 
-    // 게임을 시작한다.
     start() {
-        for (var player of this.players) {
+        for (var id in this.players) {
             // 게임 시작을 보낸다.
-            player.socket.emit('game_start', protocol.game_start());
+            this.players[id].emit('game_start', protocol.game_start());
             console.log(`Game Started`);
         }
-        
-        for (var player of this.players) {
-            // 배치
-            player.socket.on('place_done', (msg) => {
-                player.isReady = true;
-                console.log(`${player.name} is ready`);
 
-                // 모든 플레이어가 준비 상태인지 검사
-                let isAllReady = true;
-                for (var player of this.players) {
-                    isAllReady = isAllReady && player.isReady;
-                }
-                
-                // 모두 준비되면 호스트에게 턴을 넘긴다.
-                if (isAllReady) {
-                    for (var player of this.players) {
-                        player.socket.emit('place_end', protocol.place_end());
-                    }
-                    console.log('Place End');
-                    
-                    this.host.socket.emit('turn_start', protocol.turn_start());
-                    console.log(`${this.host.name}'s turn start`);
-                }
-            });
-
-            // 공격
-            player.socket.on('attack_request', (msg) => {
-                player.oppo.socket.emit('attack_forward', protocol.attack_forward(msg));
-                console.log(`${player.name}'s attack start`);
-            });
-            player.socket.on('attack_result', (msg) => {
-                player.oppo.socket.emit('attack_response', protocol.attack_result(msg));
-                console.log(`${player.name}'s attack end`);
-            });
-            
-            // 턴 종료
-            player.socket.on('turn_end', (msg) => {
-                // 게임이 끝났는지 확인한다.
-                let json = JSON.parse(msg);
-                if (json.isGameOver) {
-                    let winner = player.name;
-                    for (var player of this.players) {
-                        player.socket.emit('gameover', protocol.gameover(winner));
-                    }
-
-                    console.log(`The winner is ${winner}`);
-                }
-            });
+        // 모든 플레이어가 준비 상태인지 검사한다.
+        while (this.isAllReady() == false) {
+            // 준비가 안되었다면 대기한다.
+            sleep(10);   
         }
+        
+        // 모두 준비되면 호스트에게 턴을 넘긴다.
+        for (var id in this.players) {
+            this.players[id].emit('place_end', protocol.place_end());
+        }
+        console.log('Place End');
+        this.host.emit('turn_start', protocol.turn_start());
+        console.log(`${this.host.name}'s turn start`);
+        
+        // 게임이 끝날 때까지 대기한다.
+        while (this.isOver == false) {
+            sleep(10);
+        }
+
+        // 게임이 끝났다면 종료한다.
+        for (var id in this.players ) {
+            this.players[id].close();
+        }
+        console.log('Game Over');
+    }
+
+    checkGameOver(isOver, player) {
+        // 게임이 끝났는지 확인한다.
+        if (isOver) {
+            let winner = player.name;
+            for (var p of this.players) {
+                p.emit('gameover', protocol.gameover(winner));
+            }
+            console.log(`The winner is ${winner}`);
+        } else {
+            player.oppo.emit('turn_start', protocol.turn_start());
+            console.log(`${ player.oppo.name }'s turn start`);
+        }
+
+        this.isOver = isOver;
+    }
+
+    isAllReady() {
+        return this.host.isReady && this.host.oppo.isReady;
+    }
+
+    sleep(ms) {
+        let start = new Date().getTime();
+        while (new Date().getTime() < start + ms);
     }
 }
 module.exports.Session=Session;
