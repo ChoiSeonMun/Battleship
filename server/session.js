1ece125c1c040ea65bfdad2d2cc45f858683e098
pungun <tkaltk123@gmail.com>
2019-11-23 09:58:44 +0900

    fixed etc

--- server/session.js
+++ server/session.js
@@ -41,54 +41,111 @@ class Session {
             console.log(`Game Started`);
         }
         
-        for (var player of this.players) {
-            // 배치
-            player.socket.on('place_done', (msg) => {
-                player.isReady = true;
-                console.log(`${player.name} is ready`);
-
-                // 모든 플레이어가 준비 상태인지 검사
-                let isAllReady = true;
-                for (var player of this.players) {
-                    isAllReady = isAllReady && player.isReady;
-                }
+        // 배치
+        this.host.socket.on('place_done', (msg) => {
                 
-                // 모두 준비되면 호스트에게 턴을 넘긴다.
-                if (isAllReady) {
-                    for (var player of this.players) {
-                        player.socket.emit('place_end', protocol.place_end());
-                    }
-                    console.log('Place End');
-                    
-                    this.host.socket.emit('turn_start', protocol.turn_start());
-                    console.log(`${this.host.name}'s turn start`);
-                }
-            });
+            this.host.isReady = true;
+            console.log(`${this.host.name} is ready`);
 
-            // 공격
-            player.socket.on('attack_request', (msg) => {
-                player.oppo.socket.emit('attack_forward', protocol.attack_forward(msg));
-                console.log(`${player.name}'s attack start`);
-            });
-            player.socket.on('attack_result', (msg) => {
-                player.oppo.socket.emit('attack_response', protocol.attack_result(msg));
-                console.log(`${player.name}'s attack end`);
-            });
+            // 모든 플레이어가 준비 상태인지 검사
+            let isAllReady = true;
+            for (var player of this.players) {
+                isAllReady = isAllReady && player.isReady;
+            }
             
-            // 턴 종료
-            player.socket.on('turn_end', (msg) => {
-                // 게임이 끝났는지 확인한다.
-                let json = JSON.parse(msg);
-                if (json.isGameOver) {
-                    let winner = player.name;
-                    for (var player of this.players) {
-                        player.socket.emit('gameover', protocol.gameover(winner));
-                    }
-
-                    console.log(`The winner is ${winner}`);
+            // 모두 준비되면 호스트에게 턴을 넘긴다.
+            if (isAllReady) {
+                for (var player of this.players) {
+                    player.socket.emit('place_end', protocol.place_end());
                 }
-            });
-        }
+                console.log('Place End');
+                
+                player.socket.emit('turn_start', protocol.turn_start());
+                console.log(`${player.name}'s turn start`);
+            }
+        });
+
+        // 공격
+        this.host.socket.on('attack_request', (msg) => {
+            this.host.oppo.socket.emit('attack_forward', protocol.attack_forward(msg));
+            console.log(`${this.host.name}'s attack start`);
+        });
+        this.host.socket.on('attack_result', (msg) => {
+            this.host.oppo.socket.emit('attack_response', protocol.attack_result(msg));
+            console.log(`${this.host.name}'s attack end`);
+        });
+        
+        // 턴 종료
+        this.host.socket.on('turn_end', (msg) => {
+            // 게임이 끝났는지 확인한다.
+            let json = JSON.parse(msg);
+            if (json.IsGameOver) {
+                let winner = this.host.name;
+                for (var player of this.players) {
+                    player.socket.emit('gameover', protocol.gameover(winner));
+                }
+
+                console.log(`The winner is ${winner}`);
+            } else {
+                this.host.oppo.socket.emit('turn_start', protocol.turn_start());
+                console.log(`${this.host.name}'s turn started`);
+            }
+
+            this.isGameOver = json.IsGameOver;
+        });
+
+        // 배치
+        this.host.oppo.socket.on('place_done', (msg) => {
+                
+            this.host.oppo.isReady = true;
+            console.log(`${this.host.oppo.name} is ready`);
+
+            // 모든 플레이어가 준비 상태인지 검사
+            let isAllReady = true;
+            for (var player of this.players) {
+                isAllReady = isAllReady && player.isReady;
+            }
+            
+            // 모두 준비되면 호스트에게 턴을 넘긴다.
+            if (isAllReady) {
+                for (var player of this.players) {
+                    player.socket.emit('place_end', protocol.place_end());
+                }
+                console.log('Place End');
+                
+                this.host.socket.emit('turn_start', protocol.turn_start());
+                console.log(`${this.host.name}'s turn start`);
+            }
+        });
+
+        // 공격
+        this.host.oppo.socket.on('attack_request', (msg) => {
+            this.host.oppo.oppo.socket.emit('attack_forward', protocol.attack_forward(msg));
+            console.log(`${this.host.oppo.name}'s attack start`);
+        });
+        this.host.oppo.socket.on('attack_result', (msg) => {
+            this.host.oppo.oppo.socket.emit('attack_response', protocol.attack_result(msg));
+            console.log(`${this.host.oppo.name}'s attack end`);
+        });
+        
+        // 턴 종료
+        this.host.oppo.socket.on('turn_end', (msg) => {
+            // 게임이 끝났는지 확인한다.
+            let json = JSON.parse(msg);
+            if (json.IsGameOver) {
+                let winner = this.host.oppo.name;
+                for (var player of this.players) {
+                    player.socket.emit('gameover', protocol.gameover(winner));
+                }
+
+                console.log(`The winner is ${winner}`);
+            } else {
+                this.host.oppo.oppo.socket.emit('turn_start', protocol.turn_start());
+                console.log(`${this.host.oppo.name}'s turn started`);
+            }
+
+            this.isGameOver = json.IsGameOver;
+        });
     }
 }
 module.exports.Session=Session;
