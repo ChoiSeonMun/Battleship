fa41b5feea95b09030db3979f6af00a29efaa353
pungun <tkaltk123@gmail.com>
2019-12-05 16:52:34 +0900

    server into client

--- client/MistOcean_Cocos/assets/Scripts/Enums.js
+++
@@ -1,8 +0,0 @@
-var enums=require('../../../../Common/Enums');
-cc.EDirec = cc.Enum(enums.EDirec);
-
-cc.ShipType = cc.Enum(enums.ShipType);
-cc.ScreenType = cc.Enum(enums.ScreenType);
-cc.TileType = cc.Enum(enums.TileType);
-cc.ZOrder = cc.Enum(enums.ZOrder);
-cc.AttackEventType = cc.Enum(enums.AttackEventType);
