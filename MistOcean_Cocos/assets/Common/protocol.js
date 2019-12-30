264e4ea0d470b09459b9ead53c10789c17bf971d
choiseonmun <ch002717@gmail.com>
2019-12-30 16:13:46 +0900

    Modify protocols
    We changed protocols of this game.
    So I changed this object to suit this change.

--- MistOcean_Cocos/assets/Common/protocol.js
+++ MistOcean_Cocos/assets/Common/protocol.js
@@ -1,45 +1,96 @@
-//==============================================
-//common protocol define
-//
-const protocol = {
-    //==============================================
-    //lobby protocol
-    //
-    join_request(nickname) {//client
-        return JSON.stringify({ nickname: nickname});
+// 프로토콜을 위한 Object
+const PROTOCOLS = {
+    // Auto-matching
+    
+    /**
+     * 서버에 조인을 요청한다.
+     * @param {string} nickname 클라이언트의 닉네임 
+     * @returns {JSON} 생성된 메세지
+     */
+    joinRequest(nickname) {
+        return JSON.stringify({ nickname: nickname });
     },
-    join_response(type,info="") {//server
-        return JSON.stringify({ type:type,info:info });
+    
+    /**
+     * 클라이언트에 조인에 대한 응답을 한다.
+     * @param {int} type 응답의 타입
+     * @param {*} info 응답에 따른 추가 정보
+     * @returns {string} 생성된 메세지
+     */
+    joinResponse(type, info = "") {
+        return JSON.stringify({ type: type, info: info });
     },
-    //==============================================
-    //place protocol
-    //
-    place_done(shipinfos) {//client
-        return JSON.stringify(shipinfos);
+
+    // Place
+
+    /**
+     * 배의 배치가 완료되었음을 서버에게 알린다.
+     * @param {ShipInfo} shipInfos 클라이언트가 배치한 배의 정보
+     * @returns {string} 생성된 메세지
+     */
+    placeDone(shipInfos) {
+        return JSON.stringify({ shipinfos: shipInfos});
     },
-    place_response(R,C) {//server
-        return JSON.stringify({R:R,C:C});
+
+    /**
+     * 배치가 완료된 클라이언트에게 폭탄의 번호를 알려준다.
+     * @param {int} R 폭탄의 행 번호
+     * @param {int} C 폭탄의 열 번호
+     * @returns {string} 생성된 메세지
+     */
+    placeResponse(R, C) {
+        return JSON.stringify({ R: R, C: C});
     },
-    enermy_ready(){},//server
-    start_event(){},//server
-    //==============================================
-    //battle protocol
-    //
-    turn_event(){},//server    
-    attack_request(R,C) {//client
-        return JSON.stringify({R:R,C:C});
+
+    /**
+     * 상대가 준비되었다는 것을 다른 클라이언트에게 알린다.
+     * @param {string} nickname 상대방의 닉네임
+     * @returns {string} 생성된 메세지
+     */
+    readyEvent(nickname) {
+        return JSON.stringify({ nickname: nickname })
     },
-    attack_response(type,remain_enermy,changed_tiles) {//server
+
+    /**
+     * 클라이언트에게 게임 시작을 알린다.
+     */
+    startEvent() { },
+    
+    // Battle
+
+    /**
+     * 클라이언트에게 턴의 시작을 알린다.
+     */
+    turnEvent() { },
+    
+    /**
+     * 서버에게 공격을 요청한다.
+     * @param {int} R 공격할 행 번호 
+     * @param {int} C 공격할 열 번호
+     * @returns {string} 생성된 메세지
+     */
+    attackRequest(R, C) {
+        return JSON.stringify({ R: R, C: C});
+    },
+
+    /**
+     * 공격 성공 여부를 알린다.
+     * @param {int} result 0이면 성공. 1이면 실패다.
+     * @returns {string} 생성된 메세지
+     */
+    attackResponse(result) {
         return JSON.stringify({type:type,remain_enermy:remain_enermy,changed_tiles:changed_tiles});
     },
-    attack_event(type,remain_ship,changed_tiles){//server
-        return JSON.stringify({type:type,remain_ship:remain_ship,changed_tiles:changed_tiles});
-    },
-    game_over(winner){//server
-        return JSON.stringify({winner:winner});
+    
+    /**
+     * 승자를 알림으로써 게임을 끝낸다.
+     * @param {string} winner 승자
+     */
+    gameOver(winner) {
+        return JSON.stringify({ winner: winner });
     }
     //==============================================
 
 }
-module.exports.protocol=protocol;
-//==============================================
+
+module.exports = PROTOCOLS;
